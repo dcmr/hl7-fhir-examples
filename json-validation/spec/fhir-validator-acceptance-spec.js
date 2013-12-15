58ec6a534a84d867c69af9cfa083842aebc59316
Jeff Xiong <gigix1980@gmail.com>
2013-12-15 18:46:42 +0300

    nicer test cases

--- json-validation/spec/fhir-validator-acceptance-spec.js
+++ json-validation/spec/fhir-validator-acceptance-spec.js
@@ -4,18 +4,13 @@ describe("FHIR JSON validator acceptance", function () {
     var validator = require(__dirname + '/../js/fhir-validator');
 
     describe("Patient", function () {
-        it("validates all well-formed patient documents", function () {
-            var patientFixtureDir = __dirname + '/fixture/patient';
-            var allFixtureFileNames = fs.readdirSync(patientFixtureDir);
-            allFixtureFileNames.forEach(function (fixtureFilename) {
+        var patientFixtureDir = __dirname + '/fixture/patient';
+        var allFixtureFileNames = fs.readdirSync(patientFixtureDir);
+        allFixtureFileNames.forEach(function (fixtureFilename) {
+            it("validates patient document in file [ " + fixtureFilename + " ]", function () {
                 var patientAsJson = fs.readFileSync(patientFixtureDir + '/' + fixtureFilename);
                 var errors = validator.validate(JSON.parse(patientAsJson));
-                if (errors.length > 0) {
-                    console.error(errors);
-                    throw "Failed to validate file " + fixtureFilename;
-                } else {
-                    console.log("Validated file " + fixtureFilename);
-                }
+                expect(errors).toEqual([]);
             });
         });
     });
